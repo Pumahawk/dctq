f83840dbccc426b3a810a14a0bea091321c16bcd
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-12-15 00:57:42 +0100

    Add cors filter

--- internal/controllers/controllers.go
+++ internal/controllers/controllers.go
@@ -25,12 +25,12 @@ func NewControllerServerImpl(statusService services.StatusService, messageServic
 	messagesController := NewMessagesController(messageService, statusService)
 
 	serverMux := http.NewServeMux()
-	serverMux.Handle("GET "+StatusEndpoint, statusController.GetAll())
-	serverMux.Handle("POST "+StatusEndpoint, statusController.Create())
-	serverMux.Handle("GET "+StatusByIdEndpoint, statusController.GetById())
-	serverMux.Handle("PUT "+StatusByIdEndpoint, statusController.Update())
-	serverMux.Handle("GET "+MessagesEndpoint, statusController.Follow())
-	serverMux.Handle("POST "+MessagesEndpoint, messagesController.Send())
+	serverMux.Handle("GET "+StatusEndpoint, cors(statusController.GetAll()))
+	serverMux.Handle("POST "+StatusEndpoint, cors(statusController.Create()))
+	serverMux.Handle("GET "+StatusByIdEndpoint, cors(statusController.GetById()))
+	serverMux.Handle("PUT "+StatusByIdEndpoint, cors(statusController.Update()))
+	serverMux.Handle("GET "+MessagesEndpoint, cors(statusController.Follow()))
+	serverMux.Handle("POST "+MessagesEndpoint, cors(messagesController.Send()))
 
 	return &ControllerServerImpl{
 		ServerMux: serverMux,
@@ -39,4 +39,19 @@ func NewControllerServerImpl(statusService services.StatusService, messageServic
 
 func (c *ControllerServerImpl) ListenAndServe() error {
 	return http.ListenAndServe(":8080", c.ServerMux)
+}
+
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
